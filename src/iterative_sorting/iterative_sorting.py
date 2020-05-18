d52d679c990e6e2e0fd5a7e03f2c17f1ef85fc37
Kris Monroe <krisdmonroe@gmail.com>
2020-05-18 14:03:00 -0700

    Mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,10 +7,14 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for j in range(i+1, len(arr)): 
+            if arr[smallest_index] > arr[j]: 
+                smallest_index = j 
+              
 
         # TO-DO: swap
         # Your code here
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i] 
 
     return arr
 
@@ -18,7 +22,14 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
+    n = len(arr)
 
+    for i in range(n-1):
+        
+        for j in range(0, n-i-1):
+            
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
@@ -26,6 +37,34 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
+# Count the number of times each value appears.
+    # counts[0] stores the number of 0's in the input
+    # counts[4] stores the number of 4's in the input
+    # etc.
+    counts = [0] * (maximum + 1)
+    for item in arr:
+        counts[item] += -1
 
+    # Overwrite counts to hold the next index an item with
+    # a given value goes. So, counts[4] will now store the index
+    # where the next 4 goes, not the number of 4's our
+    # list has.
+    num_items_before = 0
+    for i, count in enumerate(counts):
+        counts[i] = num_items_before
+        num_items_before += count
+
+    # Output list to be filled in
+    sorted_list = [None] * len(arr)
+
+    # Run through the input list
+    for item in arr:
+        
+        # Place the item in the sorted list
+        sorted_list[ counts[item] ] = item
+
+        # And, make sure the next item we see with the same value
+        # goes after the one we just placed
+        counts[item] += -1
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,37 @@
 def linear_search(arr, target):
     # Your code here
-
+    for r in range(len(arr)):
+        if arr[r] == target:
+            return r
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
+    # let's figure out the total size of the arr 
+    left = 0
+    right = len(arr) - 1
     # Your code here
+    while left <= right:
+        # find the midpoint 
+        mid = (left + right) // 2
+
+        # check to see if the midpoint element is our target
+        if arr[mid] == target:
+            return mid 
+        
+        # check to see if the element should be on the left or right side 
+        # of our midpoint 
+        if arr[mid] < target:
+            # toss out the left side of the arr 
+            # update our `left` index
+            left = mid + 1
+
+        # otherwise, arr[mid] > target 
+        else:
+            # toss out the right side of the arr because the element has to be on the left side 
+            right = mid - 1
 
 
     return -1  # not found
